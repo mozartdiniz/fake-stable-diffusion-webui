834ec23ca8fc80a267ea101c87961ce630d8e621
Mozart Diniz <mozart.diniz@gmail.com>
2025-06-08 10:02:39 +0100

    echoes

---
+++ webui-user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "============================================"
+echo "executed webui-user.sh"
+echo "============================================"
+
+# ... existing code ...
+export COMMANDLINE_ARGS="--no-half --precision full" 
