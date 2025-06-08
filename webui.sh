df8024b4c621860b0b77f97e17b13d2c6035b8cc
Mozart Diniz <mozart.diniz@gmail.com>
2025-06-08 10:41:54 +0100

    debug

--- webui.sh
+++ webui.sh
@@ -4,6 +4,12 @@ echo "============================================"
 echo "executed webui.sh"
 echo "============================================"
 
+echo "============================================"
+echo "cat webui-user.sh"
+echo "============================================"
+
+cat ./webui-user.sh
+
 cd /home/stable/fake-stable-diffusion-webui/models/ESRGAN || {
   echo "❌ Falha ao entrar no diretório ESRGAN"
   exit 1
